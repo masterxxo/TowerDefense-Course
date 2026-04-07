8c2c4faa1150fbc3c5100fa867bfbb312440e1f1
Mateusz Róg <kontakt@matrog.pl>
2026-04-07 11:27:38 +0200

    Random enemy target

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
@@ -8,19 +9,59 @@ public class Tower : MonoBehaviour
    [SerializeField] private Transform towerHead;
 
    [SerializeField] private float rotationSpeed;
+   [SerializeField] private float attackRange = 1.5f;
+   [SerializeField] private LayerMask whatIsEnemy;
 
    private void Update()
    {
+      if (currentEnemy == null)
+      {
+         currentEnemy = FindRandomEnemyWithinRange();
+         return;
+      }
+
+      if (Vector3.Distance(currentEnemy.position, transform.position) > attackRange)
+      {
+         currentEnemy = null;
+      }
+      
       RotateTowardsEnemy();
+   }
+
+   private Transform FindRandomEnemyWithinRange()
+   {
+      List<Transform> possibleTargets = new List<Transform>();
+      Collider[] enemiesAround = Physics.OverlapSphere(transform.position, attackRange, whatIsEnemy);
+      foreach (Collider enemy in enemiesAround)
+      {
+         possibleTargets.Add(enemy.transform);
+      }
+
+      if (possibleTargets.Count <= 0)
+      {
+         return null;
+      }
+      
+      return possibleTargets[Random.Range(0, possibleTargets.Count)];
    }
 
    private void RotateTowardsEnemy()
    {
+      if (currentEnemy == null)
+      {
+         return;
+      }
+      
       Vector3 directionToEnemy = currentEnemy.position - towerHead.position;
 
       Quaternion lookRotation = Quaternion.LookRotation(directionToEnemy);
       
       Vector3 rotation = Quaternion.Lerp(towerHead.rotation, lookRotation, rotationSpeed * Time.deltaTime).eulerAngles;
       towerHead.rotation = Quaternion.Euler(rotation);
+   }
+
+   private void OnDrawGizmos()
+   {
+      Gizmos.DrawWireSphere(transform.position, attackRange);
    }
 }
