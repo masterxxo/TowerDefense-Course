02313cc36f46fcfda80bd24d9cdf408f22f65586
Mateusz Róg <kontakt@matrog.pl>
2026-04-29 19:52:40 +0200

    Enemy visuals for slopes

---
+++ Assets/Scripts/EnemyVisuals.cs
@@ -0,0 +1,26 @@
+using UnityEditor.Search;
+using UnityEngine;
+
+public class EnemyVisuals : MonoBehaviour
+{
+    [SerializeField] private Transform visuals;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float verticalRotationSpeed;
+
+    private void Update()
+    {
+        AlignWithSlope();
+    }
+
+    private void AlignWithSlope()
+    {
+        if (visuals == null)
+            return;
+
+        if (Physics.Raycast(visuals.position, Vector3.down, out RaycastHit hit, Mathf.Infinity, whatIsGround))
+        {
+            Quaternion targetRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+            visuals.rotation = Quaternion.Lerp(visuals.rotation, targetRotation, Time.deltaTime * verticalRotationSpeed);
+        }
+    }
+}
